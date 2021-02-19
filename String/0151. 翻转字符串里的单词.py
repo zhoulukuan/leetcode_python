4037001507b667b4bdd191eb7f7feb3f1720e55f
zhoulukuan <zhoulukuan@qq.com>
2021-02-19 13:03:58 +0800

    取消tag组织方式

--- String/0151. 翻转字符串里的单词.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def reverseWords(self, s: str) -> str:
-        words = s.split(" ")
-        ans = []
-        for word in words:
-            if len(word) > 0:
-                ans.append(word)
-        return ' '.join(ans[::-1])
