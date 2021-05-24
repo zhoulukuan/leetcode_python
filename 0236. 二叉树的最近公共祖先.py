9c0cce92912d00f1638f2b2b0e55c7b9aa3cd850
MiaCat <zhoulukuan@qq.com>
2021-05-24 21:53:52 +0800

    Create 0236. 二叉树的最近公共祖先.py

---
+++ 0236. 二叉树的最近公共祖先.py
@@ -0,0 +1,10 @@
+class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root: return
+        if root == p or root == q: return root
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if not left and not right: return
+        if not left and right: return right
+        if left and not right: return left
+        return root
