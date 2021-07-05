dbc894268dc1bcb549189f0f6151dd6dd9d79723
lity <zhoulukuan@qq.com>
2021-07-05 21:47:53 +0800

    Update 0022. 括号生成.py

--- leetcode/0022. 括号生成.py
+++ leetcode/0022. 括号生成.py
@@ -1,19 +1,14 @@
-class Solution:
-    def generateParenthesis(self, n: int) -> List[str]:
-        self.n = n
-        ans = []
-        if n == 0: return ans
-        self.helper(ans, 0, 0, '')
-        return ans
-    
-    # num1: 左括号数量  num0: 右括号数量
-    def helper(self, ans, num0, num1, curr_string):
-        if num1 == self.n:
-            curr_string = curr_string + ')' * (num1 - num0)
-            ans.append(curr_string)
-            return
-        
-        self.helper(ans, num0, num1 + 1, curr_string + '(')
-        if num1 > num0:
-            self.helper(ans, num0 + 1, num1, curr_string + ')')
-        return 
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        ans = []
+        self.dfs(0, 0, n, "", ans)
+        return ans
+
+    def dfs(self, nl, nr, n, path, ans):
+        if nr > nl: return
+        if nl == n: 
+            ans.append(path + ")" * (n - nr))
+            return
+        else:
+            self.dfs(nl + 1, nr, n, path + "(", ans)
+            self.dfs(nl, nr + 1, n, path + ")", ans)
