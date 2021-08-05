82a94fe12a8c3d2bbb9ea646debca8d296edd445
lity <zhoulukuan@qq.com>
2021-08-05 11:26:39 +0800

    Update 0017. 电话号码的字母组合.py

--- leetcode/0017. 电话号码的字母组合.py
+++ leetcode/0017. 电话号码的字母组合.py
@@ -1,24 +1,19 @@
-class Solution:
-    def letterCombinations(self, digits: str) -> List[str]:
-        self.digits = digits
-        self.num2char = {
-            '2': "abc",
-            '3': "def",
-            '4': "ghi",
-            '5': "jkl",
-            '6': "mno",
-            '7': "pqrs",
-            '8': "tuv",
-            '9': "wxyz"
-        }
-        ans = []
-        if len(self.digits) == 0: return ans
-        self.helper(ans, "", 0)
-        return ans
+class Solution:
+    def __init__(self):
+        self.num2char = {2:'abc', 3:'def', 4:'ghi', 5:'jkl', 6:'mno', 7:'pqrs', 8:'tuv', 9:'wxyz'}
 
-    def helper(self, ans, s, i):
-        if len(s) == len(self.digits):
-            ans.append(s)
-            return
-        for char in self.num2char[self.digits[i]]:
-            self.helper(ans, s + char, i + 1)
+    def letterCombinations(self, digits: str) -> List[str]:
+        ans = []
+        if len(digits) == 0: return ans
+
+        def backtracking(path, i):
+            if i == len(digits):
+                ans.append(''.join(path))
+                return
+            for char in self.num2char[int(digits[i])]:
+                path.append(char)
+                backtracking(path, i + 1)
+                path.pop()
+
+        backtracking([], 0)
+        return ans
