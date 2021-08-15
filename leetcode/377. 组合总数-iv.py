43e3c67d0d664bbbc4ea1cecee825aa885f54478
lity <zhoulukuan@qq.com>
2021-08-15 16:28:36 +0800

    Create 377. 组合总数-iv.py

---
+++ leetcode/377. 组合总数-iv.py
@@ -0,0 +1,10 @@
+from collections import defaultdict
+class Solution:
+    def combinationSum4(self, nums, target: int) -> int:
+        dp = [0] * (target + 1)
+        dp[0] = 1
+        for i in range(1, target + 1):
+            for num in nums:
+                if i >= num:
+                    dp[i] += dp[i - num]
+        return dp[-1]
