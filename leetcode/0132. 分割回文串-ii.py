3e492a219790149190ab3ff23d948a29c3a07f3f
lity <zhoulukuan@qq.com>
2021-08-26 22:51:04 +0800

    Update 0132. 分割回文串-ii.py

--- leetcode/0132. 分割回文串-ii.py
+++ leetcode/0132. 分割回文串-ii.py
@@ -13,12 +13,17 @@ class Solution:
                         dp[i][j] = True
         if dp[0][n - 1]: return 0
 
+        # 动态规划
         cut = [0 for _ in range(n)]
         for i in range(n):
+            # 如果0~i是回文,不需要切割,次数为0
             if dp[0][i]:
                 cut[i] = 0
             else:
                 max_time = i
+                # 如果0~i不是回文,可以通过如下途径变成回文
+                # 对于0<=j<i, 可以分为0~j和j+1~i两段,如果j+1~i是回文,则可以通过0~j状态进行状态转移
+                # 备注: 不需要考虑j+1~i不是回文的情况,因为j变大的时候会覆盖这种情况
                 for j in range(i):
                     if dp[j + 1][i]:
                         max_time = min(max_time, cut[j] + 1)
