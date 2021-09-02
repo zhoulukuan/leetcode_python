e9cfed59dbf498a8fcc87e18cf2244d970052202
lity <zhoulukuan@qq.com>
2021-09-02 13:55:36 +0800

    Update 0072. 编辑距离.py

--- leetcode/0072. 编辑距离.py
+++ leetcode/0072. 编辑距离.py
@@ -3,16 +3,18 @@ class Solution:
         n1 = len(word1)
         n2 = len(word2)
         dp = [[0 for _ in range(n2 + 1)] for _ in range(n1 + 1)]
+        word1 = ['#'] + [c for c in word1]
+        word2 = ['#'] + [c for c in word2]
+
         for i in range(n1 + 1):
-            dp[i][0] = i
-        for j in range(n2 + 1):
-            dp[0][j] = j
-
-        for i in range(1, n1 + 1):
-            for j in range(1, n2 + 1):
-                if word1[i - 1] == word2[j - 1]:
-                    dp[i][j] = dp[i - 1][j - 1]
+            for j in range(n2 + 1):
+                if i == 0:
+                    dp[i][j] = j
+                elif j == 0:
+                    dp[i][j] = i
                 else:
-                    dp[i][j] = min(dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]) + 1
-
+                    if word1[i] == word2[j]:
+                        dp[i][j] = min(dp[i-1][j-1], dp[i-1][j] + 1, dp[i][j-1]+1)
+                    else:
+                        dp[i][j] = min(dp[i-1][j-1], dp[i-1][j], dp[i][j-1]) + 1
         return dp[-1][-1]
