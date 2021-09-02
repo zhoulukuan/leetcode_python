83a6f159781a011c0b42df8d45cdc019eadb122d
lity <zhoulukuan@qq.com>
2021-09-02 13:56:30 +0800

    Create 0198. 打家劫舍.py

---
+++ leetcode/0198. 打家劫舍.py
@@ -0,0 +1,11 @@
+class Solution:
+    def rob(self, nums) -> int:
+        n = len(nums)
+        if n == 1: return nums[0]
+        dp = [0 for _ in range(n)]
+
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, n):
+            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        return dp[n - 1]
