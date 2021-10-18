50cae01f2f518a67cd4d14570b956d56c599492d
lity <zhoulukuan@qq.com>
2021-10-18 21:44:27 +0800

    Update 0034. 在排序数组中查找元素的第一个和最后一个位置.py

--- leetcode/0034. 在排序数组中查找元素的第一个和最后一个位置.py
+++ leetcode/0034. 在排序数组中查找元素的第一个和最后一个位置.py
@@ -1,27 +1,25 @@
-class Solution:
-    def searchRange(self, nums: List[int], target: int) -> List[int]:
-        lo = 0
-        hi = len(nums)
-        if hi == 0: return [-1, -1]
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if nums[mid] < target:
-                lo = mid + 1
-            else:
-                hi = mid
-        if lo < len(nums) and nums[lo] == target:
-            start = lo
-        else:
-            return [-1, -1]
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        n = len(nums)
+        if n < 1 or target < nums[0] or target > nums[-1]: return [-1, -1]
+        
+        lo, hi = 0, n
+        while lo < hi:
+            mid = (lo + hi) >> 1
+            if nums[mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+        p1 = lo
+        if nums[p1] != target:
+            return [-1, -1]
 
-        lo = 0
-        hi = len(nums)
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if nums[mid] <= target:
-                lo = mid + 1
-            else:
-                hi = mid
-        end = lo - 1
-
-        return [start, end]
+        lo, hi = 0, n
+        while lo < hi:
+            mid = (lo + hi) >> 1
+            if nums[mid] <= target:
+                lo = mid + 1
+            else:
+                hi = mid
+        p2 = lo - 1
+        return [p1, p2]
