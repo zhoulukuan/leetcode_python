5d1021929669bb60469f2f00bed8d5ea109a75f0
lity <zhoulukuan@qq.com>
2021-10-19 22:23:19 +0800

    Update 0033. 搜索旋转排序数组.py

--- leetcode/0033. 搜索旋转排序数组.py
+++ leetcode/0033. 搜索旋转排序数组.py
@@ -1,22 +1,33 @@
 class Solution:
-    def search(self, nums, target):
-        lo, hi = 0, len(nums) - 1
-        while lo <= hi:
-            mid = lo + (hi - lo) // 2
-            # 先处理中间值
-            if nums[mid] == target:
-                return mid
+    def search(self, nums, target: int) -> int:
+        lo, hi = 0, len(nums)
+        while lo < hi:
+            mid = (lo + hi) >> 1
+            if nums[lo] < nums[mid]:    # 左半段有序
+                if nums[lo] <= target <= nums[mid]:
+                    return self.binary_search(nums, lo, mid + 1, target)
+                else:
+                    lo = mid + 1
+            else:                       # 右半段有序
+                if nums[mid] <= target <= nums[hi - 1]:
+                    return self.binary_search(nums, mid, hi, target)
+                else:
+                    hi = mid
 
-            # mid在左段
-            if nums[lo] <= nums[mid]:
-                if nums[lo] <= target < nums[mid]:
-                    hi = mid - 1
-                else:
-                    lo = mid + 1
-            # mid在右段
+        if lo < len(nums) and nums[lo] == target:
+            return lo
+        else:
+            return -1
+
+    def binary_search(self, nums, lo, hi, target):
+        while lo < hi:
+            mid = (lo + hi) >> 1
+            if nums[mid] < target:
+                lo = mid + 1
             else:
-                if nums[mid] < target <= nums[hi]:
-                    lo = mid + 1
-                else:
-                    hi = mid - 1
-        return -1
+                hi = mid
+
+        if lo < len(nums) and nums[lo] == target:
+            return lo
+        else:
+            return -1
