a8eef375dc8c988e8012f7a71fa13619a48ca81e
lity <zhoulukuan@qq.com>
2021-10-20 21:44:50 +0800

    Update 1095. 山脉数组中查找目标值.py

--- leetcode/1095. 山脉数组中查找目标值.py
+++ leetcode/1095. 山脉数组中查找目标值.py
@@ -4,30 +4,28 @@ class Solution:
         return self.find(mountain_arr, 0, n - 1, target)
 
     def find(self, mountain_arr, lo, hi, target):
-        while lo < hi:
-            mid = (lo + hi) >> 1
-            vm = mountain_arr.get(mid)
-            nv = mountain_arr.get(mid + 1)
-            # lo~mid段递增
-            if vm < nv:
-                # target若大于vm则不可能在左半段,没有查找的必要
-                if target <= vm:
-                    index = self.binary_search(mountain_arr, lo, mid + 1, target, 1)
-                    if index != -1: return index
-                lo = mid + 1
-            # mid~hi段递减
-            else:
-                # 递归
-                index = self.find(mountain_arr, lo, mid, target)
+        if lo == hi:
+            return lo if mountain_arr.get(lo) == target else -1
+        mid = (lo + hi) >> 1
+        vm = mountain_arr.get(mid)
+        nv = mountain_arr.get(mid + 1)
+        # lo~mid段递增
+        if vm < nv:
+            # target若大于vm则不可能在左半段,没有查找的必要
+            if target <= vm:
+                index = self.binary_search(mountain_arr, lo, mid + 1, target, 1)
                 if index != -1: return index
-                # target若大于nv则不可能在右半段,没有查找的必要
-                if target > nv:
-                    return -1
-                else:
-                    return self.binary_search(mountain_arr, mid + 1, hi + 1, target, -1)
-        return lo if mountain_arr.get(lo) == target else -1
+            return self.find(mountain_arr, mid + 1, hi, target)
+        # mid~hi段递减
+        else:
+            index = self.find(mountain_arr, lo, mid, target)
+            if index != -1: return index
+            # target若大于nv则不可能在右半段,没有查找的必要
+            if target > nv:
+                return -1
+            else:
+                return self.binary_search(mountain_arr, mid + 1, hi + 1, target, -1)
 
-    # 带符号的二分法
     def binary_search(self, nums, lo, hi, target, flag):
         n = hi
         while lo < hi:
