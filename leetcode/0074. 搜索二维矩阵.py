416077219ee76e5338bf8519e095e83e3d2612cc
lity <zhoulukuan@qq.com>
2021-10-21 22:11:41 +0800

    Update 0074. 搜索二维矩阵.py

--- leetcode/0074. 搜索二维矩阵.py
+++ leetcode/0074. 搜索二维矩阵.py
@@ -1,28 +1,24 @@
-class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        m = len(matrix)
-        if m == 0: return False
-        n = len(matrix[0])
-        if n == 0: return False
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
+        lo, hi = 0, m
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if matrix[mid][0] <= target:
+                lo = mid + 1
+            else:
+                hi = mid
+        if lo == 0:
+            return False
+        else:
+            row = lo - 1
 
-        lo, hi = 0, m
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if matrix[mid][0] == target: return True
-            if matrix[mid][0] <= target:
-                lo = mid + 1
-            else:
-                hi = mid
-        line = lo - 1
-        
-        lo, hi = 0, n
-        while lo < hi:
-            mid = lo + (hi - lo) // 2
-            if matrix[line][mid] == target: return True
-            if matrix[line][mid] < target:
-                lo = mid + 1
-            else:
-                hi = mid
-        if lo < n and matrix[line][lo] == target:
-            return True
-        return False
+        lo, hi = 0, n
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if matrix[row][mid] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+        return True if lo < n and matrix[row][lo] == target else False
