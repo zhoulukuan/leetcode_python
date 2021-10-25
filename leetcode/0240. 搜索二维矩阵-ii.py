6bbc5ce46416f61fdc54c060ca3b8449dd194f2b
lity <zhoulukuan@qq.com>
2021-10-25 11:25:31 +0800

    Update 0240. 搜索二维矩阵-ii.py

--- leetcode/0240. 搜索二维矩阵-ii.py
+++ leetcode/0240. 搜索二维矩阵-ii.py
@@ -1,18 +1,33 @@
 class Solution:
-    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
-        n = len(matrix)
-        if n == 0: return False
-        m = len(matrix[0])
-        if m == 0: return False
+    def searchMatrix(self, matrix, target: int) -> bool:
+        m = len(matrix)
+        n = len(matrix[0])
 
-        # 沿对角线搜索
-        row = 0
-        col = m - 1
-        while row < n and col >= 0:
-            num = matrix[row][col]
-            if num == target:
+        # 先锁定所在的行数范围,加快搜索
+        lo, hi = 0, m
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if matrix[mid][0] <= target:
+                lo = mid + 1
+            else:
+                hi = mid
+        up_bounder = lo
+
+        lo, hi = 0, m
+        while lo < hi:
+            mid = lo + (hi - lo) // 2
+            if matrix[mid][n - 1] < target:
+                lo = mid + 1
+            else:
+                hi = mid
+        
+        # 不去探索两个边界值直接在矩阵中沿45度对角线搜索也可以
+        row, col = lo, n - 1
+        while row < up_bounder and col >= 0:
+            val = matrix[row][col]
+            if val == target:
                 return True
-            elif num < target:
+            elif val < target:
                 row += 1
             else:
                 col -= 1
