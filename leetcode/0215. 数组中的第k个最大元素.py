d97b3109338b737aa54ee8b745647f25bea204bb
lity <zhoulukuan@qq.com>
2021-11-03 14:41:50 +0800

    Update 0215. 数组中的第k个最大元素.py

--- leetcode/0215. 数组中的第k个最大元素.py
+++ leetcode/0215. 数组中的第k个最大元素.py
@@ -1,22 +1,28 @@
+import random
 class Solution:
-    def findKthLargest(self, nums: List[int], k: int) -> int:
-        return self.helper(nums, 0, len(nums) - 1, k)
+    def findKthLargest(self, nums, k: int) -> int:
+        n = len(nums)
+        if n < k: return -1
+        return self.quickSortHelper(nums, 0, n - 1, k)
 
-    def helper(self, nums, start, end, k):
-        if start > end:
-            return -1
+    def quickSortHelper(self, nums, lo, hi, k):
+        if lo > hi: return -1
+        if lo == hi: return nums[lo]
+        rand = random.randint(lo, hi + 1)
+        nums[rand], nums[hi] = nums[hi], nums[rand]
 
-        value = nums[end]
-        boundary = start
-        for i in range(start, end):
-            if nums[i] < value:
-                nums[i], nums[boundary] = nums[boundary], nums[i]
+        val = nums[hi]
+        index = lo
+        boundary = lo
+        while index < hi:
+            if nums[index] > val:
+                nums[index], nums[boundary] = nums[boundary], nums[index]
                 boundary += 1
-        nums[boundary], nums[end] = nums[end], nums[boundary]
-
-        if end - boundary > k - 1:
-            return self.helper(nums, boundary + 1, end, k)
-        elif end - boundary == k - 1:
+            index += 1
+        nums[index], nums[boundary] = nums[boundary], nums[index]
+        if boundary == k - 1:
             return nums[boundary]
+        elif boundary < k - 1:
+            return self.quickSortHelper(nums, boundary + 1, hi, k)
         else:
-            return self.helper(nums, start, boundary - 1, k - (end - boundary + 1))
+            return self.quickSortHelper(nums, lo, boundary, k)
