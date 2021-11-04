40c4f897b3fd75fffecec5451cf7a30f5c87f777
lity <zhoulukuan@qq.com>
2021-11-04 11:43:14 +0800

    Update 0004. 寻找两个正序数组的中位数.py

--- leetcode/0004. 寻找两个正序数组的中位数.py
+++ leetcode/0004. 寻找两个正序数组的中位数.py
@@ -2,44 +2,63 @@ class Solution:
     def findMedianSortedArrays(self, nums1, nums2) -> float:
         n = len(nums1)
         m = len(nums2)
-        num1 = (n + m - 1) // 2 + 1
-        num2 = (n + m) // 2 + 1
-        if num1 == num2:
-            return self.getKth(nums1, 0, n - 1, nums2, 0, m - 1, num1)
+        if (m + n) % 2 == 1:
+            k = (m + n + 1) // 2
+            index1, index2 = self.helper(nums1, nums2, k - 1, 0, 0)
+            # 数组元素个数是奇数,剔除k-1个数后直接返回较小值
+            if index1 == n:
+                return nums2[index2]
+            elif index2 == m:
+                return nums1[index1]
+            else:
+                return min(nums1[index1], nums2[index2])
         else:
-            return self.getKth(nums1, 0, n - 1, nums2, 0, m - 1, num1) * 0.5 + self.getKth(nums1, 0, n - 1, nums2, 0, m - 1, num2) * 0.5
+            k = (m + n) // 2
+            index1, index2 = self.helper(nums1, nums2, k - 1, 0, 0)
+            # 数组个数是偶数,要从index1和index2出发得到两个最小的值
+            if index1 == n:
+                v1, v2 = nums2[index2], nums2[index2 + 1]
+            elif index2 == m:
+                v1, v2 = nums1[index1], nums1[index1 + 1]
+            else:
+                if nums1[index1] <= nums2[index2]:
+                    v1 = nums1[index1]
+                    index1 += 1
+                else:
+                    v1 = nums2[index2]
+                    index2 += 1
+                if index1 == n:
+                    v2 = nums2[index2]
+                elif index2 == m:
+                    v2 = nums1[index1]
+                else:
+                    v2 = min(nums1[index1], nums2[index2])
+            return float(v1 + v2) / 2
 
-    # 辅助函数: 从序列中选择第num小的数
-    def getKth(self, nums1, start1, end1, nums2, start2, end2, num):
-        len1 = end1 - start1 + 1
-        len2 = end2 - start2 + 1
-        if len1 > len2:
-            # 保持nums1的长度是中小于nums2,避免多分支的情况
-            return self.getKth(nums2, start2, end2, nums1, start1, end1, num)
-        if len1 == 0:
-            # nums1已经没有元素,直接从nums2中选择
-            return nums2[start2 + num - 1]
+    # 辅助函数,从数组中剔除前k个最小的数后指针所在位置
+    def helper(self, nums1, nums2, k, index1, index2):
+        # 考察某个数组已经剔除完的情况
+        if index1 == len(nums1):
+            return index1, index2 + k
+        if index2 == len(nums2):
+            return index1 + k, index2
 
-        # k=1的时候,无法再对半减少,直接返回两个数列最小的那个
-        if num == 1:
-            return min(nums1[start1], nums2[start2])
+        # 递归终止点
+        # k=0,直接返回;k=1,剔除开头最小元素后返回
+        if k == 1:
+            return (index1 + 1, index2) if nums1[index1] <= nums2[index2] else (index1, index2 + 1)
+        if k == 0:
+            return index1, index2
 
-        # 每次对半选择
-        # 从nums1和nums2中各取前k元素,因为共有2k<=num个元素,看哪边小就丢掉哪边
-        # 所以2k中最大的元素会被保留下来,而num则会减少k个元素
-        # 举例nums1=[1,4,6],nums2=[2,5,7,10],num=6,k=3
-        # nums1中选择了1,4,6,nums2中选择了2,5,7,由于6<7,所以把nums1中的3个都丢掉,这里第6小的元素也就是7一定不会丢掉
-        k = num // 2
-        if len1 >= k:
-            p1, n1 = start1 + k - 1, nums1[start1 + k - 1]
-            p2, n2 = start2 + k - 1, nums2[start2 + k - 1]
+        # 每次剔除k//2个元素
+        n = k // 2
+            # 数组长度可能不够n,计算每个数组能剔除的最多的数
+        n1 = min(n, len(nums1) - index1)
+        n2 = min(n, len(nums2) - index2)
+        if nums1[index1 + n1 - 1] <= nums2[index2 + n2 - 1]:
+            k -= n1
+            p1, p2 = self.helper(nums1, nums2, k, index1 + n1, index2)
         else:
-            # 若nums1长度小于k的特殊情况
-            p1, n1 = end1, nums1[end1]
-            p2, n2 = start2 + num - len1 - 1, nums2[start2 + num - len1 - 1]
-
-
-        if n1 <= n2:
-            return self.getKth(nums1, p1 + 1, end1, nums2, start2, end2, num - (p1 - start1 + 1))
-        else:
-            return self.getKth(nums1, start1, end1, nums2, p2 + 1, end2, num - (p2 - start2 + 1))
+            k -= n2
+            p1, p2 = self.helper(nums1, nums2, k, index1, index2 + n2)
+        return p1, p2
