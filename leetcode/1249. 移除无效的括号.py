c134a3b798cf93347e4188128d32116822b3bd35
lity <zhoulukuan@qq.com>
2021-12-13 19:32:38 +0800

    Update 1249. 移除无效的括号.py

--- leetcode/1249. 移除无效的括号.py
+++ leetcode/1249. 移除无效的括号.py
@@ -10,9 +10,12 @@ class Solution:
                 pos.append(i)
             elif char == ')':
                 if right >= left:
+                    # 右括号多于左括号直接删除
                     rm.add(i)
                 else:
+                    # 否则进行计数
                     right += 1
+        # 左括号多于右括号,则删除多于的括号,遍历的时候pos已经记录了位置
         if left > right:
             for _ in range(left - right):
                 rm.add(pos.pop())
