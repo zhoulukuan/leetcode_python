247e1b7cd83c6d94895c2294c035a16588311da6
lity <zhoulukuan@qq.com>
2021-12-26 22:20:12 +0800

    Update 0264. 丑数-ii.py

--- leetcode/0264. 丑数-ii.py
+++ leetcode/0264. 丑数-ii.py
@@ -9,9 +9,12 @@ class Solution:
         for i in range(1, n):
             ugly = min(nums[i2] * 2, nums[i3] * 3, nums[i5] * 5)
             nums.append(ugly)
-            if (nums[i] == nums[i2] * 2): i2 += 1
-            if (nums[i] == nums[i3] * 3): i3 += 1
-            if (nums[i] == nums[i5] * 5): i5 += 1
+            if nums[i] == nums[i2] * 2: 
+                i2 += 1
+            if nums[i] == nums[i3] * 3: # 不能使用else if,因为会存在3*2和2*3为一个数的情况
+                i3 += 1
+            if nums[i] == nums[i5] * 5: 
+                i5 += 1
         return nums[n - 1]
 
     # 方法2: 小顶堆
