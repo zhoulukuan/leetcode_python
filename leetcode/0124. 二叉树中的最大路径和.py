d064e287435d5a1cfb7be5cd0fb791549858b6de
lity <zhoulukuan@qq.com>
2021-12-30 21:43:08 +0800

    Update 0124. 二叉树中的最大路径和.py

--- leetcode/0124. 二叉树中的最大路径和.py
+++ leetcode/0124. 二叉树中的最大路径和.py
@@ -1,16 +1,22 @@
 class Solution:
+    def __init__(self) -> None:
+        self.ans = -1000
+
     def maxPathSum(self, root: TreeNode) -> int:
-        self.ans = -10000
         self.helper(root)
         return self.ans
 
     def helper(self, root):
-        if root == None: return -10000
-        leftMax = self.helper(root.left)
-        rightMax = self.helper(root.right)
-        data = [leftMax, rightMax, root.val, root.val + leftMax, root.val + rightMax, root.val + leftMax + rightMax, self.ans]
-        self.ans = max(data)
-        if leftMax < 0 and rightMax < 0:
-            return root.val
-        else:
-            return root.val + max(leftMax, rightMax)
+        if root is None: return 0
+        left = self.helper(root.left)
+        right = self.helper(root.right)
+
+        maxV = root.val
+        # 如果左树或者右树>0,累加进当前的根节点值中
+        if left > 0:
+            maxV += left
+        if right > 0:
+            maxV += right
+        self.ans = max(self.ans, maxV)
+        # 根节点如果<0,则不是必须选取的,可以舍弃
+        return max(max(left, right) + root.val, 0)
