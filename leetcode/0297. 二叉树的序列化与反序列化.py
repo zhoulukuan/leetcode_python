2b0864f5fa4a26db3bedbb5d2aed6ed6bed28c4a
lity <zhoulukuan@qq.com>
2022-02-28 22:17:29 +0800

    Update 0297. 二叉树的序列化与反序列化.py

--- leetcode/0297. 二叉树的序列化与反序列化.py
+++ leetcode/0297. 二叉树的序列化与反序列化.py
@@ -6,17 +6,29 @@ class Codec:
         :type root: TreeNode
         :rtype: str
         """
-        if root == None: return [-1001]
-        ans = [root.val]
-        ans = ans + self.serialize(root.left)
-        ans = ans + self.serialize(root.right)
-        return ans
-
+        def helper(root, ans):
+            if not root:
+                ans.append('X')
+                return
+            ans.append(str(root.val))
+            helper(root.left, ans)
+            helper(root.right, ans)
+        ans = []
+        helper(root, ans)
+        return "_".join(ans)
 
     def deserialize(self, data):
-        val = data.pop(0)
-        if val == -1001: return None
-        root = TreeNode(val)
-        root.left = self.deserialize(data)
-        root.right = self.deserialize(data)
-        return root
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        def helper(data):
+            num = data.pop(0)
+            if num == 'X':
+                return None
+            root = TreeNode(num)
+            root.left = helper(data)
+            root.right = helper(data)
+            return root
+        return helper(data.split("_"))
