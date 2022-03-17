4b0da743a419a845ed0d87e6538dc93c817d70cb
lity <zhoulukuan@qq.com>
2022-03-17 14:04:36 +0800

    Update 0542. 01-矩阵.py

--- leetcode/0542. 01-矩阵.py
+++ leetcode/0542. 01-矩阵.py
@@ -1,36 +1,25 @@
 from collections import deque
 class Solution:
-    def updateMatrix(self, board):
-        """
-        Do not return anything, modify board in-place instead.
-        """
-        m = len(board)
-        if m == 0: return None
-        n = len(board[0])
-        if n == 0: return None
+    def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
+        m = len(mat)
+        n = len(mat[0])
+        ans = [[-1 for _ in range(n)] for _ in range(m)]
 
-        visited = [[0 for _ in range(n)] for _ in range(m)]
-        res = [[0 for _ in range(n)] for _ in range(m)]
-        queue = deque()
+        q = deque()
         for i in range(m):
             for j in range(n):
-                if board[i][j] == 0:
-                    queue.append([i, j])
-                    visited[i][j] = 1
-
-        surround = [[0, 1], [0, -1], [1, 0], [-1, 0]]
-        level = 0
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                x, y = queue.popleft()
-                if board[x][y] == 1:
-                    res[x][y] = level
-                for dx, dy in surround:
+                if mat[i][j] == 0:
+                    q.append((i, j))
+                    ans[i][j] = 0
+        curr_level = 1
+        while q:
+            curr_num = len(q)
+            for _ in range(curr_num):
+                x, y = q.popleft()
+                for dx, dy in ((-1, 0), (1, 0), (0, -1), (0, 1)):
                     new_x, new_y = x + dx, y + dy
-                    if new_x < 0 or new_x >= m or new_y < 0 or new_y >= n or visited[new_x][new_y] == 1:
-                        continue
-                    queue.append([new_x, new_y])
-                    visited[new_x][new_y] = 1
-            level += 1
-        return res
+                    if 0 <= new_x < m and 0 <= new_y < n and ans[new_x][new_y] == -1:
+                        ans[new_x][new_y] = curr_level
+                        q.append((new_x, new_y))
+            curr_level += 1
+        return ans
