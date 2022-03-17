a3be1e4c2ecf64a8afc2869c915653b2a341891e
lity <zhoulukuan@qq.com>
2022-03-17 10:23:04 +0800

    Update 1091. 二进制矩阵中的最短路径.py

--- leetcode/1091. 二进制矩阵中的最短路径.py
+++ leetcode/1091. 二进制矩阵中的最短路径.py
@@ -1,25 +1,23 @@
 from collections import deque
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m = len(grid)
-        n = len(grid[0])
-        surrounds = [[-1, 0], [1, 0], [0, 1], [0, -1], [1, 1], [1, -1], [-1, 1], [-1, -1]]
-        queue = deque()
-        visited = [[False for _ in range(n)] for _ in range(m)]
+        n = len(grid)
+        if grid[0][0] != 0 or grid[n - 1][n - 1] != 0: return -1
 
-        if grid[0][0] != 0: return -1
-        level = 1
-        queue.append([0, 0])
-        visited[0][0] = True
-        while queue:
-            size = len(queue)
-            for _ in range(size):
-                x, y = queue.popleft()
-                if x == m - 1 and y == n - 1: return level
-                for dx, dy in surrounds:
+        q = deque()
+        q.append((0, 0))
+        curr_level = 1
+        while q:
+            curr_num = len(q)
+            for _ in range(curr_num):
+                x, y = q.popleft()
+                if x == n - 1 and y == n - 1:
+                    return curr_level
+                for dx, dy in ((-1, -1), (-1, 0), (0, -1), (-1, 1),
+                            (1, 1), (1, 0), (0, 1), (1, -1)):
                     new_x, new_y = x + dx, y + dy
-                    if 0 <= new_x < m and 0 <= new_y < n and visited[new_x][new_y] == False and grid[new_x][new_y] == 0:
-                        queue.append([new_x, new_y])
-                        visited[new_x][new_y] = True
-            level += 1
+                    if 0 <= new_x < n and 0 <= new_y < n and grid[new_x][new_y] == 0:
+                        q.append((new_x, new_y))
+                        grid[new_x][new_y] = 2
+            curr_level += 1
         return -1
