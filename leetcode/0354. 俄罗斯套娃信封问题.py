e5ecfa0230126e4478609ef64d40892841cb79c4
lity <zhoulukuan@qq.com>
2022-09-18 22:14:42 +0800

    Update 0354. 俄罗斯套娃信封问题.py

--- leetcode/0354. 俄罗斯套娃信封问题.py
+++ leetcode/0354. 俄罗斯套娃信封问题.py
@@ -10,3 +10,22 @@ class Solution:
                     dp[i] = dp[j] + 1
             if dp[i] > ans: ans = dp[i]
         return ans
+
+class Solution:
+    def maxEnvelopes(self, envelopes):
+        envelopes.sort(key=lambda x : (x[0], -x[1]))
+        dp = []
+        for i in range(len(envelopes)):
+            target = envelopes[i][1]
+            if not dp or target > dp[-1]:
+                dp.append(target)
+            lo = 0
+            hi = len(dp)
+            while lo < hi:
+                mid = lo + (hi - lo) // 2
+                if dp[mid] < target:
+                    lo = mid + 1
+                else:
+                    hi = mid
+            dp[lo] = target
+        return len(dp)
