e9df331f55eba12e20a6932bec266b63dbc6c900
lity <zhoulukuan@qq.com>
2022-09-27 14:19:28 +0800

    Update 0639. 解码方法-ii.py

--- leetcode/0639. 解码方法-ii.py
+++ leetcode/0639. 解码方法-ii.py
@@ -1,24 +1,32 @@
 class Solution:
-    def numDecodings(self, s: str) -> int:
-        MOD = 10 ** 9 + 7
-        n = len(s)
-        dp = [1, 1] + [0 for _ in range(n)]
-        s = '99' + s
+    def numDecodings(self, s):
+        if s[0] == '0':
+            return 0
 
-        for i in range(2, n + 2):
-            if s[i] == '*':
+        n = len(s)
+        dp = [0 for _ in range(n+1)]
+        dp[0] = 1
+        dp[1] = 1 if s[0] != '*' else 9
+        for i in range(2, n + 1):
+            if s[i - 1] == '*':
                 dp[i] += dp[i - 1] * 9
-                if s[i - 1] == '1' or s[i - 1] == '*':
+            elif '1' <= s[i - 1] <= '9':
+                dp[i] += dp[i - 1]
+
+            if s[i - 1] == '*' and s[i - 2] == '*':
+                dp[i] += dp[i - 2] * 15
+            elif s[i - 1] == '*':
+                if s[i - 2] == '1':
                     dp[i] += dp[i - 2] * 9
-                if s[i - 1] == '2' or s[i - 1] == '*':
+                elif s[i - 2] == '2':
                     dp[i] += dp[i - 2] * 6
-            else:
-                if s[i] != '0':
-                    dp[i] += dp[i - 1]
-                if s[i - 1] == '1' or (s[i - 1] == '2' and '0' <= s[i] <= '6'):
+            elif s[i - 2] == '*':
+                if s[i - 1] <= '6':
+                    dp[i] += dp[i - 2] * 2
+                else:
                     dp[i] += dp[i - 2]
-                if s[i - 1] == '*':
-                    if s[i] <= '6': dp[i] += dp[i - 2] * 2
-                    else: dp[i] += dp[i - 2]
-            dp[i] = dp[i] % MOD
+            elif 10 <= int(s[i-2:i]) <= 26:
+                dp[i] += dp[i - 2]
+
+            dp[i] = dp[i] % (10**9 + 7)
         return dp[-1]
