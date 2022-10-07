72370e92f33d4d202e0b145fe34b72e3b6fcd15b
lity <zhoulukuan@qq.com>
2022-10-07 20:30:08 +0800

    Update 0213. 打家劫舍-ii.py

--- leetcode/0213. 打家劫舍-ii.py
+++ leetcode/0213. 打家劫舍-ii.py
@@ -1,16 +1,18 @@
 class Solution:
-    def rob(self, nums) -> int:
-        n = len(nums)
-        if n == 1: return nums[0]
-        return max(self.helper(nums[1:]), self.helper(nums[:n - 1]))
+    def rob(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            return max(self.helper(nums[1:]), self.helper(nums[:-1]))
 
-    def helper(self, nums) -> int:
-        n = len(nums)
-        if n == 1: return nums[0]
-        dp = [0 for _ in range(n)]
-
-        dp[0] = nums[0]
-        dp[1] = max(nums[0], nums[1])
-        for i in range(2, n):
-            dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
-        return dp[n - 1]
+    def helper(self, nums):
+        if len(nums) <= 2:
+            return max(nums)
+        a = nums[0]
+        b = max(nums[1], nums[0])
+        ans = b
+        for i in range(2, len(nums)):
+            curr = max(a + nums[i], b)
+            a, b = b, curr
+            ans = max(ans, curr)
+        return ans
